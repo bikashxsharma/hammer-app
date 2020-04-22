eda7f0a88937c69e0b9ccc092c67e5c2a820bc00
Bikash Sharma-home-pc <bikashxsharma@gmail.com>
2020-04-22 22:34:50 +0300

    redux dev tool extension compose added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "./css/main.css"
-import {createStore, applyMiddleware} from "redux"
+import {createStore, applyMiddleware, compose} from "redux"
 import {Provider} from "react-redux"
 import allReducers from "./reducers"
 import thunk from "redux-thunk"
 
-const store = createStore(allReducers, applyMiddleware(thunk))
+const store = createStore(
+  allReducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
